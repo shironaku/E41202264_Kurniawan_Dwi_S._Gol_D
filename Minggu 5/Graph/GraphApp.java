4796379123661f0589a8c02a810c48955fbbd4e7
shironaku <80763830+shironaku@users.noreply.github.com>
2021-04-10 20:24:13 +0700

    Add files via upload

---
+++ Minggu 5/Graph/GraphApp.java
@@ -0,0 +1,24 @@
+
+public class GraphApp {
+    public static void main(String[] args) {
+        Graph theGraph = new Graph();
+
+        theGraph.addVertex('A'); // 0
+        theGraph.addVertex('B'); // 1
+        theGraph.addVertex('C'); // 2
+        theGraph.addVertex('D'); // 3
+        theGraph.addVertex('E'); // 4
+        
+        theGraph.addEdge(0, 1); // AB
+        theGraph.addEdge(1, 2); // BC
+        theGraph.addEdge(2, 3); // AD
+        theGraph.addEdge(3, 4); // DE
+        
+        theGraph.bfs();
+        
+//        theGraph.display();
+    }
+
+    
+}
+
